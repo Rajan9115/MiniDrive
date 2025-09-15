d8216d18488cb4323726aad123993f03a247e147
Rajan Kumar <rajank5038383@gmail.com>
2025-09-16 00:29:41 +0530

    DB configured

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 
 function connectToDB(){
-    mongoose.connect(process.env.MONGO_URI).then(()=>{
+mongoose.connect(process.env.MONGO_URI,{
+      useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
        console.log('Connected to database') 
     })
 }
